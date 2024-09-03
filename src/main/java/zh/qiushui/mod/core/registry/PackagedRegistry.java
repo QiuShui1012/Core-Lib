6e9c17d6be5a0ecbaf047d4994b3b72259da0542
QiuShui1012 <150409561+QiuShui1012@users.noreply.github.com>
2024-09-04 02:40:31 +0800

    使注册时传入的identifier可自定义

--- src/main/java/zh/qiushui/mod/core/registry/PackagedRegistry.java
+++ src/main/java/zh/qiushui/mod/core/registry/PackagedRegistry.java
@@ -6,40 +6,49 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import zh.qiushui.mod.core.QSCoreUtil;
 import zh.qiushui.mod.core.object.StoredFlowableFluid;
 
+import java.util.function.Function;
+
 public class PackagedRegistry {
-    public static Block block(String path, Block block) {
-        return Registry.register(Registries.BLOCK, QSCoreUtil.idCore(path), block);
+    private final Function<String, Identifier> function;
+
+    public PackagedRegistry(Function<String, Identifier> function) {
+        this.function = function;
     }
 
-    public static Item item(String path, Item item) {
-        return Registry.register(Registries.ITEM, QSCoreUtil.idCore(path), item);
+    public Block block(String path, Block block) {
+        return Registry.register(Registries.BLOCK, this.function.apply(path), block);
     }
 
-    public static ItemGroup itemGroup(String path, ItemGroup itemGroup) {
-        return Registry.register(Registries.ITEM_GROUP, QSCoreUtil.idCore(path), itemGroup);
+    public Item item(String path, Item item) {
+        return Registry.register(Registries.ITEM, this.function.apply(path), item);
     }
 
-    public static Fluid fluid(String path, Fluid fluid) {
-        return Registry.register(Registries.FLUID, QSCoreUtil.idCore(path), fluid);
+    public ItemGroup itemGroup(String path, ItemGroup itemGroup) {
+        return Registry.register(Registries.ITEM_GROUP, this.function.apply(path), itemGroup);
     }
-    public static <S extends Fluid, F extends Fluid> StoredFlowableFluid<S, F> flowableFluid(String pathFluid, S fluidStill, F fluidFlowing) {
+
+    public Fluid fluid(String path, Fluid fluid) {
+        return Registry.register(Registries.FLUID, this.function.apply(path), fluid);
+    }
+    public <S extends Fluid, F extends Fluid> StoredFlowableFluid<S, F> flowableFluid(String pathFluid, S fluidStill, F fluidFlowing) {
         return flowableFluid(pathFluid, "flowing_" + pathFluid, fluidStill, fluidFlowing);
     }
 
-    public static <S extends Fluid, F extends Fluid> StoredFlowableFluid<S, F> flowableFluid(String pathStill, String pathFlowing, S fluidStill, F fluidFlowing) {
+    public <S extends Fluid, F extends Fluid> StoredFlowableFluid<S, F> flowableFluid(String pathStill, String pathFlowing, S fluidStill, F fluidFlowing) {
         S still = flowableFluidStill(pathStill, fluidStill);
         F flowing = flowableFluidFlowing(pathFlowing, fluidFlowing);
         return new StoredFlowableFluid<>(still, flowing);
     }
 
-    public static <S extends Fluid> S flowableFluidStill(String path, S fluidStill) {
-        return Registry.register(Registries.FLUID, QSCoreUtil.idCore(path), fluidStill);
+    public <S extends Fluid> S flowableFluidStill(String path, S fluidStill) {
+        return Registry.register(Registries.FLUID, this.function.apply(path), fluidStill);
     }
 
-    public static <F extends Fluid> F flowableFluidFlowing(String path, F fluidFlowing) {
-        return Registry.register(Registries.FLUID, QSCoreUtil.idCore(path), fluidFlowing);
+    public <F extends Fluid> F flowableFluidFlowing(String path, F fluidFlowing) {
+        return Registry.register(Registries.FLUID, this.function.apply(path), fluidFlowing);
     }
 }
